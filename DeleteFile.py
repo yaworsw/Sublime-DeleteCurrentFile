5eeaf9e38d747ae81ccbe9efc8880ebd9e2ee511
William Yaworsky <wj.px01@gmail.com>
2013-07-19 09:27:50 -0400

    Added settings to customize the plugin
    auto_close_buffer    -> determines if the current buffer should be closed automatically
    prompt_before_delete -> determines if a prompt should be displayed asking if the user is sure they want to delete the file

--- DeleteFile.py
+++ DeleteFile.py
@@ -1,9 +1,21 @@
 import sublime, sublime_plugin, os
 
+settings = sublime.load_settings('DeleteFile.sublime-settings')
+
 class DeleteFileCommand(sublime_plugin.TextCommand):
   def run(self, edit):
+
     window = sublime.active_window()
     view   = sublime.Window.active_view(window)
-    if os.path.isfile(view.file_name()):
-      os.remove(view.file_name())
+
+    file   = view.file_name()
+
+    if settings.get('prompt_before_delete', False):
+      if not sublime.ok_cancel_dialog('Are you sure you want to delete \'%s\'?' % file):
+        return
+
+    if (file != None and os.path.isfile(view.file_name())):
+      os.remove(file)
+
+    if settings.get('auto_close_buffer', True):
       window.run_command('close')

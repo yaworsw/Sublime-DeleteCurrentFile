e21059818f655ce71509ea3cd88539fb0a6b5f9a
William Yaworsky <wj.px01@gmail.com>
2013-07-19 11:18:13 -0400

    added params to override user settings

--- DeleteFile.py
+++ DeleteFile.py
@@ -3,19 +3,25 @@ import sublime, sublime_plugin, os
 settings = sublime.load_settings('DeleteFile.sublime-settings')
 
 class DeleteFileCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
+  def run(self, edit, prompt_before_delete=None, auto_close_buffer=None):
+
+    if prompt_before_delete == None:
+      prompt_before_delete = settings.get('prompt_before_delete', False)
+
+    if auto_close_buffer == None:
+      auto_close_buffer = settings.get('auto_close_buffer', True)
 
     window = sublime.active_window()
     view   = sublime.Window.active_view(window)
 
     file   = view.file_name()
 
-    if settings.get('prompt_before_delete', False):
+    if prompt_before_delete:
       if not sublime.ok_cancel_dialog('Are you sure you want to delete \'%s\'?' % file):
         return
 
     if (file != None and os.path.isfile(view.file_name())):
       os.remove(file)
 
-    if settings.get('auto_close_buffer', True):
+    if auto_close_buffer:
       window.run_command('close')

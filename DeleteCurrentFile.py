23421975acbe4d9b19642ad3df1711b497f06152
Martin van Harmelen <Martin@vanharmelen.com>
2018-02-20 12:19:11 +0545

    pep8 fix

--- DeleteCurrentFile.py
+++ DeleteCurrentFile.py
@@ -1,43 +1,47 @@
-import sublime, sublime_plugin, os
+import sublime
+import sublime_plugin
+import os
 
 settings = None
 
+
 def plugin_loaded():
-  update_settings()
-  settings.add_on_change('extensions_path', update_settings)
+    update_settings()
+    settings.add_on_change('extensions_path', update_settings)
+
 
 def update_settings():
-  global settings
-  settings = sublime.load_settings('DeleteCurrentFile.sublime-settings')
+    global settings
+    settings = sublime.load_settings('DeleteCurrentFile.sublime-settings')
 
 
 class DeleteCurrentFileCommand(sublime_plugin.TextCommand):
 
-  def run(self, edit, prompt_before_delete=None, auto_close_buffer=None):
+    def run(self, edit, prompt_before_delete=None, auto_close_buffer=None):
 
-    global settings
-    settings
+        global settings
+        settings
 
-    if prompt_before_delete == None:
-      prompt_before_delete = settings.get('prompt_before_delete', False)
+        if prompt_before_delete is None:
+            prompt_before_delete = settings.get('prompt_before_delete', False)
 
-    if auto_close_buffer == None:
-      auto_close_buffer = settings.get('auto_close_buffer', True)
+        if auto_close_buffer is None:
+            auto_close_buffer = settings.get('auto_close_buffer', True)
 
-    window = sublime.active_window()
-    view   = sublime.Window.active_view(window)
+        window = sublime.active_window()
+        view = sublime.Window.active_view(window)
+        file_name = view.file_name()
+        message = "Are you sure you want to delete '{}'?".format(file_name)
 
-    file   = view.file_name()
+        if prompt_before_delete:
+            if not sublime.ok_cancel_dialog(message):
+                return
 
-    if prompt_before_delete:
-      if not sublime.ok_cancel_dialog('Are you sure you want to delete \'%s\'?' % file):
-        return
+        if view.is_dirty():
+            view.set_scratch(True)
 
-    if view.is_dirty():
-      view.set_scratch(True)
+        if auto_close_buffer:
+            window.run_command('close_file')
 
-    if auto_close_buffer:
-      window.run_command('close_file')
-
-    if (file != None and os.path.isfile(view.file_name())):
-      os.remove(file)
+        if (file_name is not None and os.path.isfile(view.file_name())):
+            os.remove(file_name)
